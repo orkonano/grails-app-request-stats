2a371fa1ec6b40f30b093ad5f525a9858b38bf21
Orko <orquito@gmail.com>
2016-02-01 06:42:05 -0300

    migrate-grails3.1: migrate to grails 3.1

--- src/main/groovy/grails/app/request/stats/GrailsAppRequestStatsGrailsPlugin.groovy
+++ src/main/groovy/grails/app/request/stats/GrailsAppRequestStatsGrailsPlugin.groovy
@@ -6,7 +6,7 @@ import grails.util.Holders
 class GrailsAppRequestStatsGrailsPlugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.0 > *"
+    def grailsVersion = "3.1 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp",
